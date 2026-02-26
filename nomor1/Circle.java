9d2458b80f60afcdcb748536d5fa1c43d6ada1c5
Bimo Surya Anggara <anggarabimo16@gmail.com>
2026-02-26 20:48:48 +0700

    Assalamualaikum done

---
+++ nomor1/Circle.java
@@ -0,0 +1,47 @@
+package nomor1;
+public class Circle {
+    private double radius;
+    private String color;
+
+    // Constructors
+    public Circle() {
+        this.radius = 1.0;
+        this.color = "red";
+    }
+
+    public Circle(double r) {
+        this.radius = r;
+        this.color = "red";
+    }
+
+    public Circle(double radius, String color) {
+        this.radius = radius;
+        this.color = color;
+    }
+
+    // Getter dan Setter untuk color
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double getArea() {
+        return radius * radius * Math.PI;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle[radius=" + radius + ", color=" + color + "]";
+    }
+}   
