bf7441c4874248e1f3a6ddbdc482e9800cbbe5e3
Bimo Surya Anggara <anggarabimo16@gmail.com>
2026-03-05 06:20:32 +0700

    fix w4 folder

---
+++ w4/Nomor2/Shape.java
@@ -0,0 +1,18 @@
+package Nomor2;
+
+public abstract class Shape
+{
+    protected String shapeName;
+
+    public Shape(String name)
+    {
+        shapeName = name;
+    }
+
+    public abstract double area();
+
+    public String toString()
+    {
+        return shapeName;
+    }
+}
